bfbf8123bac356b8444b35e9aa052644ab00e231
Samuel Petitti <sampjam@gmail.com>
2019-02-04 14:37:02 -0500

    1 test

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,15 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
-
+	
+	public String fizzBuzz(int number) {
+		if(number == 0) {
+			return "0";
+		} else {
+			if(number == 1) {
+				return "1";
+			}
+		}
+		return "";
+	} 
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,22 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
-
+	
+	@Test
+	public void Fizz_Buzz_Test_0() {
+		Assert.assertEquals("That is not the expected result", "0", new KataFizzBuzz().fizzBuzz(0));
+	}
+	@Test
+	public void Fizz_Buzz_Test_1() {
+		Assert.assertEquals("That is not the expected result", "1", new KataFizzBuzz().fizzBuzz(1));
+	}
+	
+	
+	
 }
+
+//Write a program that returns numbers as Strings, but only if the number is between 1 and 100. 
+//But for multiples of three return "Fizz" instead of the number and for the multiples of five return "Buzz".
+//For numbers which are multiples of both three and five return "FizzBuzz?".
+//Write tests that call a single methodString fizzBuzz(int number).
+//Given a positive integer input n, return the FizzBuzz output as a string.
