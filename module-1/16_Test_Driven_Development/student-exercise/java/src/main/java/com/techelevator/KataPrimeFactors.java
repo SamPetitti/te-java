d7608bfe7cf5dfa2b2c598b46c6f3b8c5f7b5007
Samuel Petitti <sampjam@gmail.com>
2019-02-04 20:44:50 -0500

    prime factors finished

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPrimeFactors.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPrimeFactors.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
-public class KataPrimeFactors {
+import java.util.ArrayList;
+import java.util.List;
 
+public class KataPrimeFactors {
+	
+	public List<Integer> factorize(int number){
+		 List<Integer> primeFactors = new ArrayList<Integer>();
+		 int i = 2;
+		 while(i <= number) {
+			if(number % i == 0) {
+				primeFactors.add(i);
+				number = number/i;
+			} else {
+				i++;
+			}
+		} return primeFactors;
+	}
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPrimeFactorsTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPrimeFactorsTest.java
@@ -1,8 +1,63 @@
 package com.techelevator;
 
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static org.junit.Assert.*;
 
 public class KataPrimeFactorsTest {
+	@Test
+	public void kata_Prime_Factors_Test_2() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2) , new KataPrimeFactors().factorize(2));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_3() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(3) , new KataPrimeFactors().factorize(3));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_2_2() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2,2), new KataPrimeFactors().factorize(4));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_2_3() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2, 3) , new KataPrimeFactors().factorize(6));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_7() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(7), new KataPrimeFactors().factorize(7));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_7_2_2() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2,2,2) , new KataPrimeFactors().factorize(8));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_3_3() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(3,3) , new KataPrimeFactors().factorize(9));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_2_5() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2, 5) , new KataPrimeFactors().factorize(10));
+	}
+	@Test
+	public void kata_Prime_Factors_Test_2_7_23() {
+	Assert.assertEquals("That is not the expected result", Arrays.asList(2, 7, 23) , new KataPrimeFactors().factorize(322));
+	}
+	
+	
+	
 
 }
+
+//Factorize a positive integer number into its prime factors.
+//Use the TDD approach to write tests that call a single method factorize(). Given a positive integer input n, return its prime factors.
+//1 is always omitted from the result set.
+//Sample Output
+//2 -> returns [2]
+//3 -> returns [3]
+//4 -> returns [2, 2]
+//6 -> returns [2, 3]
+//7 -> returns [7]
+//8 -> returns [2, 2, 2]
+//9 -> returns [3, 3]
+//10 -> returns [2, 5]
